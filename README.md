115ee4308a0f426abec9a25e0ef33a3446821960
Lena Erickson <lenacerickson@gmail.com>
2021-08-30 12:37:53 -0500

    added flat discount rate to am sched

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # finance-playground
- 
+An umbrella repository for practicing financial calculations in Python
+1. Amortization: simple fixed-rate installment loan in a class called AmSchedule, payment calculated based on [this article](http://people.cs.uchicago.edu/~jagolbec/cspp536/amort/)
--- amortization_schedule.py
+++ amortization_schedule.py
@@ -17,11 +17,24 @@ class AmSchedule:
         monthly_balance = P
         for n in range(0,N):
             monthly_interest_paid = monthly_balance * i
-            montlhly_principal_paid = self.PMT - monthly_interest_paid
+            monthly_principal_paid = self.PMT - monthly_interest_paid
             self.I = self.I + monthly_interest_paid
-            monthly_balance = monthly_balance - montlhly_principal_paid
-
+            monthly_balance = monthly_balance - monthly_principal_paid
+    
+    def presentValue(self, discount_rate):
+        """Takes as input a constant discount rate
+        Generates compound discount factors which are used to calculate present value
+        Returns present value stated as a % of the loan amount, e.g. 98.5 returned means 98.5% of P
+        """
+        CDF = 1
+        PV = 0
+        for n in range (0, self.N):
+            CDF = CDF / (1+discount_rate)
+            PV = PV + CDF * self.PMT
+        PV_pts = PV / self.P *100
+        return PV_pts
 
 myAmSchedule = AmSchedule(235000,360,.0375/12)
 print("PMT: "+str(myAmSchedule.PMT))
 print("Total Interest Paid: "+str(myAmSchedule.I))
+print("Present Value (5 pct discount rate): "+str(round(myAmSchedule.presentValue(.05/12),4)))
