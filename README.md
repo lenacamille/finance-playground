72cda476f68b9496a97ef51cec564e834429926c
Lena DeLozier <lenadelozier@gmail.com>
2023-02-04 09:40:23 -0600

    more library practice

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # finance-playground
-An umbrella repository for practicing financial calculations in Python
+An umbrella repository for practicing financial calculations, typically in Python
 1. Amortization: simple fixed-rate installment loan in a class called AmSchedule, payment calculated based on [this article](http://people.cs.uchicago.edu/~jagolbec/cspp536/amort/)
+
+
+Resources for future projects:
+- [Marketwatch Economy Calendar](https://www.marketwatch.com/economy-politics/calendar)
+- [Census Economic Indicator Calendar](https://www.census.gov/economic-indicators/calendar-listview.html)
--- library_practice.py
+++ library_practice.py
@@ -4,8 +4,29 @@ import pandas as pd
 # a = np.array([1,2,3,4,5,6,7,8])
 # print(np.mean(a))
 
+#### IMPORTING THE DATA
 url = 'cartwheel_data.csv'
 df = pd.read_csv(url)
-
+# We created a pandas dataframe object. 
 print(type(df))
+
+#### VIEWING THE DATA
+
+# This prints the first 5 rows. Good for getting a feel for the data.
 print(df.head())
+
+# This prints the whole dataframe. Not recommended for large datasets.
+# print(df)
+
+# Show a list of the column names
+print(df.columns)
+
+# Show the numver of rows and columns in the data
+print(df.shape)
+
+# Let's splice up the data
+# print(df.loc(0,'Height'))
+
+# import statistics
+# year1_data = [-15.6,7.8,5.3,-2.4,-4.0,5.3,-2.1,1.8,3.2,4.2,-2.2]
+# print(statistics.median(year1_data),statistics.mode(year1_data))
